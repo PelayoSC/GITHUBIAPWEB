e9215796574607bf5239f105157145f167ebb545
PelayoSC <pelayosc73@educastur.es>
2024-10-11 12:42:32 +0200

    practica variables

---
+++ variables.prueba.2.php
@@ -0,0 +1,5 @@
+<?php
+$variable1 = "Hola Mundo";
+echo "$variable1"; //mostraría hola mundo
+echo '$variable1'; //mostraría $variable1
+?>
---
+++ variables.prueba.php
@@ -0,0 +1,10 @@
+<?php
+$variable1 = 5;
+var_dump ($variable1);
+echo "<br>";
+$variable1 = 7.25;
+var_dump ($variable1);
+echo phpversion(); //Me da la versión de PHP
+echo "<br>";
+echo phpinfo(); 
+?>
