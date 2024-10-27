f7e1f24c5b34fe02d07e342f0ff012bedbebdd9c
PelayoSC <pelayosc73@educastur.es>
2024-10-27 17:24:08 +0100

    Casa 27/10/2024 - Unidad2Practica2

--- Unidad2Practica2/Ejercicio05.php
+++ Unidad2Practica2/Ejercicio05.php
@@ -1,7 +1,19 @@
 <?php
 /*
+5. Escribe un script PHP que muestre las tablas de multiplicar del 1 al 10. Utiliza un bucle for
 */
 
+echo "<table border='1'>";
 
+for ($numero = 1; $numero <= 10; $numero++) {
+    echo "<tr><th colspan='2'>Tabla del $numero</th></tr>";
+
+    for ($i = 1; $i <= 10; $i++) {
+        $resultado = $numero * $i;
+        echo "<tr><td>$numero x $i</td><td>$resultado</td></tr>";
+    }
+}
+
+echo "</table>";
 
 ?>
--- Unidad2Practica2/Ejercicio07.php
+++ Unidad2Practica2/Ejercicio07.php
@@ -1,7 +1,17 @@
 <?php
 /*
+7. Escribe un script PHP que muestre los números del 1 al 10 en una tabla de una fila y 10 
+columnas. Utiliza un bucle while
 */
 
+echo "<table border='1'><tr>";
 
+$i = 1;
+while ($i <= 10) {
+    echo "<td>$i</td>";
+    $i++;
+}
+
+echo "</tr></table>";
 
 ?>
